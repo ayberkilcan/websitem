7b6b8b25b008d49506769b379b76df690c3603e1
Ayberk <ayberkilcan2003@gmail.com>
2025-07-02 11:05:55 +0300

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 }
+
+const params = new URLSearchParams(window.location.search);
+if (params.get("success") === "true") {
+  const message = document.getElementById("success-message");
+  if (message) {
+    message.style.display = "block";
+    setTimeout(() => {
+      message.style.display = "none";
+    }, 5000);
+    const newURL = window.location.href.split("?")[0];
+    window.history.replaceState({}, document.title, newURL);
+  }
+}
